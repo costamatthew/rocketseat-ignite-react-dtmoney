a45de23bab0c5275fffb2bf4579ba14a38f27c6d
@costamatthew <mmateusdsilvacosta@outlook.com>
2021-08-31 18:25:21 -0300

    feat: Componente: TransactionsTable

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,14 @@
 import dtLogo from "../../assets/logo.svg";
 
-import { Container, Content } from "./style";
+import { Container, Content } from "./styles";
 
 export function Header() {
   return (
     <Container>
       <Content>
-      <img src={dtLogo} alt="dt money" />
+        <img src={dtLogo} alt="dt money" />
 
-      <button type="button">Nova Transação</button>
+        <button type="button">Nova Transação</button>
       </Content>
     </Container>
   );

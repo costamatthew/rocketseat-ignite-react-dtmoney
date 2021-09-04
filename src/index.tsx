0231b39e9f9ba316b119ee80948010611a253e69
@costamatthew <mmateusdsilvacosta@outlook.com>
2021-09-03 23:09:32 -0300

    feat: Listando transações e seeds

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelance de website",
+          type: "deposit",
+          category: "Dev",
+          amount: 7000,
+          createdAt: new Date("2021-09-01 23:00:00"),
+        },
+        {
+          id: 2,
+          title: "Alugel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1200,
+          createdAt: new Date("2021-09-02 09:00:00"),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = "api";
 

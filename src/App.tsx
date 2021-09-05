040d12bb92baf9090b29f6c6a25f88b2b8f04d39
@costamatthew <mmateusdsilvacosta@outlook.com>
2021-09-05 14:08:33 -0300

    feat: Criando hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 import { NewTransactionModal } from "./components/NewTransactionsModal";
 
-import { TransactionsProvider } from "./TransactionsContext";
+import { TransactionsProvider } from "./hooks/useTransactions";
 
 import Modal from "react-modal";
 import { GlobalStyle } from "./styles/global";
--- src/components/NewTransactionsModal/index.tsx
+++ src/components/NewTransactionsModal/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext, FormEvent } from "react";
+import { useState, FormEvent } from "react";
 import Modal from "react-modal";
 
 import closeImg from "../../assets/close.svg";
@@ -7,7 +7,7 @@ import outcomeImg from "../../assets/outcome.svg";
 
 import { Container, TransactionTypeContainer, RadioBox } from "./styles";
 
-import { TransactionsContext } from "../../TransactionsContext";
+import { useTransactions } from "../../hooks/useTransactions";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -18,7 +18,7 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
-  const { createTransaction } = useContext(TransactionsContext);
+  const { createTransaction } = useTransactions();
 
   const [title, setTitle] = useState("");
   const [amount, setAmount] = useState(0);
